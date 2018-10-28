c989f55bdafdcd01c3e7118b6d64da0515a6df7e
maaax128 <44034742+maaax128@users.noreply.github.com>
2018-10-28 13:45:52 +0500

    Add files via upload

---
+++ visa.php
@@ -0,0 +1,27 @@
+<?php
+
+$country = $argv[1];
+if (empty($argv[1])) {
+    exit('Ошибка! Аргументы не заданы. …');
+}
+
+if ($fp = fopen("https://data.gov.ru/opendata/7704206201-country/data-20180609T0649-structure-20180609T0649.csv?encoding=UTF-8", "r") or $fp = fopen("https://raw.githubusercontent.com/netology-code/php-2-homeworks/master/files/countries/opendata.csv", "r")) {
+    while ($data = fgetcsv($fp, 0, ",")){
+        $list[] = $data;
+    }    
+
+    foreach ($list as $key => $value) {
+         $column1[] = $value[1];
+         $column4[] = $value[4];
+    }
+    $registry = array_combine($column1, $column4);
+    foreach ($registry as $key2 => $key3) {
+        if ($key2[1] === $country[1]){
+        echo "$country: $key3";
+        break;         
+   
+        }            
+    }   
+}    
+
+?>
